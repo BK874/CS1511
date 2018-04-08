4eb36cd524a3593219ff68665ad3aa810963e853
Brett Schreiber <schreiberbrett@gmail.com>
2018-04-08 13:13:05 -0400

    python code

--- hw31.py
+++ hw31.py
@@ -8,34 +8,40 @@
 # 1 0 1
 # or : 101000101
 
-from random import randint
+from itertools import product
 
-def coin_flip():
-    return randint(0, 1)
+def create_pcp(quadeq_solution):
+    '''Given a quadeq solution of the form '0010101',
+    where the ith bit represents u_i = 0 or 1,
+    return the bitstring of the probablistically checkable proof'''
 
-def n_random_bits(n):
-    return ''.join(str(coin_flip())
-                   for _ in range(n))
+    return hadamard_encoding_of(quadeq_solution) \
+           + hadamard_encoding_of(tensor_product(quadeq_solution, quadeq_solution))
 
-def randomly_include_from(sequence):
-    return [element
-            for element in sequence
-            if coin_flip()]
+def hadamard_encoding_of(bitstring):
+    bitlength = len(bitstring)
 
-def verify(QUADEQ_instance, number_of_variables, bitlength, proof_string):
-    random_equations = randomly_include_from(QUADEQ_instance)
-    f, g = proof_string[:2**bitlength + 1], proof_string[2**bitlength + 1:]
+    return ''.join(inner_product(bitstring, i)
+                   for i in all_bitstrings_of_length(bitlength))
+
+def inner_product(bitstring_x, bitstring_y):
+    result = 0
+    for bit_x, bit_y in zip(bitstring_x, bitstring_y):
+        if bit_x == '1' and bit_y == '1':
+            result += 1
+
+    return str(result % 2)
+
+def tensor_product(bitstring_x, bitstring_y):
+    return ''.join('1' if bit_x == '1' and bit_y == '1' else '0'
+                   for bit_x, bit_y in product(bitstring_x, bitstring_y))
+
+def all_bitstrings_of_length(bitlength):
+    '''When bitlength = 3, returns ['000', '001', '010', ... '111']'''
+    return [to_bitstring(i, bitlength)
+            for i in range(2 ** bitlength)]
 
 
+def to_bitstring(x, bitlength):
+    return bin(x)[2:].rjust(bitlength, '0')
 
-
-
-def verify_f(bitlength):
-    for _ in range(10):
-        query_f(f, 10)
-
-def verify_g_encodes_u(u, g, bitlength):
-    for _ in range(10):
-        x, y = n_random_bits(bitlength), n_random_bits(bitlength)
-
-        
